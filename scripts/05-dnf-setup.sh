92b08edb1607aa556eb2977fce58d415b84df0da
voxyl <145951985+3aTup0k@users.noreply.github.com>
2026-07-12 20:14:23 +1000

    build fixes

--- scripts/05-dnf-setup.sh
+++ scripts/05-dnf-setup.sh
@@ -2,20 +2,30 @@
 set -e
 
 FEDORA_VERSION="${FEDORA_VERSION:-42}"
+ARCH="aarch64"
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] [05] Configuring DNF repos"
 
 cat > rootdir/etc/yum.repos.d/fedora.repo << EOF
 [fedora]
-name=Fedora \$releasever - aarch64
-metalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-\$releasever&arch=aarch64
+name=Fedora \$releasever - $ARCH
+metalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-\$releasever&arch=$ARCH
 enabled=1
 gpgcheck=0
 skip_if_unavailable=False
 
 [updates]
-name=Fedora \$releasever - aarch64 - Updates
-metalink=https://mirrors.fedoraproject.org/metalink?repo=updates-released-f\$releasever&arch=aarch64
+name=Fedora \$releasever - $ARCH - Updates
+metalink=https://mirrors.fedoraproject.org/metalink?repo=updates-released-f\$releasever&arch=$ARCH
+enabled=1
+gpgcheck=0
+skip_if_unavailable=False
+EOF
+
+cat > rootdir/etc/yum.repos.d/pocketblue.repo << EOF
+[pocketblue]
+name=pocketblue common
+baseurl=https://download.copr.fedorainfracloud.org/results/pocketblue/common/fedora-\$releasever-$ARCH/
 enabled=1
 gpgcheck=0
 skip_if_unavailable=False
